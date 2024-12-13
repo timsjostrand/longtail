3513279a87df011679ecc70b88760de8e2b8059b
Tim Sjöstrand <timsjostrand@gmail.com>
2024-12-13 10:02:06 +0100

    dist_cmake: cleanup

--- dist_cmake.bat
+++ dist_cmake.bat
@@ -6,6 +6,5 @@ cmake --version >/nul 2>&1 || (
 )
 set OUT=".cmake"
 cmake --install "%OUT%" --prefix "dist\dist-win32-x64" --config Debug
-REM cmake --install "%OUT%" --prefix "dist\dist-win32-x64" --config Release
 cmake --install "%OUT%" --prefix "dist\dist-win32-x64" --config RelWithDebInfo
 pause
